b1b2ec5e48bb2e044584f822021ab4fcd2931b07
Sberry5 <shannon.berryheller@gmail.com>
2017-10-08 21:29:34 -0500

    base complete

---
+++ assets/javascript/game.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+
+//Global Variables
+	var questions = [{
+		question: "Who made the lyrics "What you want, Baby, I got it" famous"
+		answers: ["Diana Ross", "Aretha Franklin", "Gladys Knight", "Tammi Terrell"],
+		correctAnswer: 3
+		}
+
+	var correctAnswer = 0;
+
+
+//Document ready function to wrap all javascript
+
+
+//Function to .push questions to HTML element, way to hide questions 
+//and push questions in order
+$("#answer").on("click", function(){
+	
+}
+ 
+//Function to set timer, within the function above?
+ 
+//Function to indicate correct answer
+ 
+//Corresponding answers - use "input type="radio"
+//to allow user to select option create on.click event
+//when final question is answered show user score
+
+//On.click event 
+ 
+//Loop to check userAnswer against correctAnswer
+ 
+//Conditionals
+//            if (userAnswer === correctAnswer) {
+//                            wins++;
+//                            reset();
+//            {
+//            if (userAnswer !=  correctAnswer) {
+//                            losses++;
+//                            CALL FUNCTION TO INDICATE CORRECT ANSWER
+//                            reset();
+//            {
+//Use setInterval method to timeout if user does not guess and call
+//the function to update the question and possible answers
+//Function to show quiz is complete, show # of correct answers
+//# number of incorrect answers, # of unanswered questions
+
+});
