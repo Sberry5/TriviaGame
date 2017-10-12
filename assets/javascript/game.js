4b89bd6efec95a1fb1f67118781776e1f1c672eb
Sberry5 <shannon.berryheller@gmail.com>
2017-10-11 21:13:17 -0500

    Q, A, countdown to DOM

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,30 +1,36 @@
 //Document ready function to wrap all javascript
 $(document).ready(function(){
-	$(gameQuestion).hide();
 
 //Global Variables
-	var questions = {
+	var question1 = {
     question: "Who made the lyrics 'What you want, Baby, I got it' famous?",
     choices: ["Diana Ross", "Aretha Franklin", "Gladys Knight", "Tammi Terrell"],
     correctAnswer: 1
 	};
 	
-	// var question2 = 
-	{
-    question: "Which of the following songs was sung by Marvin Gaye?",
+	var question2 = {
+    question: "Which of the following songs was sung by Marvin Gaye&#63",
     choices: ["If I Were a Boy", "Tracks of My Tears", "Isn't She Lovely", "Heard It Through The Grapevine"],
     correctAnswer: 3
 	};
 
-	// var question3 = 
-	{
-    question: "Who of the following is the founder of Motown?",
+	var question3 = {
+    question: "Who of the following is the founder of Motown&#63",
     choices: ["Berry Gordy", "Tracks of My Tears", "Phil Spector", "Heard It Through The Grapevine"],
     correctAnswer: 0
 	};
 
+
 console.log(question1.question);
 console.log(question1.choices);
+
+	var firstQuestion = (question1.question);
+	var firstChoices = (question1.choices);
+	var firstAnswer = (question1.correctAnswer);
+
+console.log(firstQuestion);
+console.log(firstChoices);
+console.log(firstAnswer);
 
 	var wins = 0;
 	var losses = 0;
@@ -33,26 +39,60 @@ console.log(question1.choices);
 
 //Reset
 
-//Function to show first question and possible answers
-// when the start button is pressed
-$("button").click(function(){
-	$("button").hide();
-	$(gameQuestion).show();
-	$("#gameQuestion").text(question1.question);
-	
-	for (var i = 0; i < question1.choices.length; i++){
-		answers=question1.choices[i];
-		$("answers").map(question1.choices[i]);
-		};
-	});
+//Function update screen with first question and possible answers
 
-//Function to .push questions to HTML element, way to hide questions 
-//and push questions in order
-// $("div.question").append(".questions.question");
-// console.log(question);
- 
-//Function to set timer, within the function above?
- 
+$("button").on("click", function(){
+		$("button").hide();
+		$("#answers").html(firstQuestion);
+
+		for (var i = 0; i < firstChoices.length; i++){
+		//Call timer function
+		run();
+          // Create button for each div-
+          // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+          var a = $("<button>");
+          // Adding input type=radio to each button
+//          a.each("input type", "radio");
+          // Adding a class of "answers" to each answer
+          a.addClass("answers");
+          // Adding a data-attribute
+          a.attr("data-name", firstChoices[i]);
+          // Providing the initial button text
+          a.text(firstChoices[i]);
+          // Adding the button to the buttons-view div
+          $("#answers").append(a);
+        }
+      });
+
+//Function to set timer, called within the starting function above
+ 	var number = 100;
+	    function run() {
+      intervalId = setInterval(decrement, 1000);
+    }
+
+    // Function to countdown
+    function decrement() {
+    	$("#timer").html(number);
+      //  Decrease number by one.
+      	number--;
+      //  Show the number in the #show-number tag.
+      $("#show-number").html("<h2>" + number + "</h2>");
+      	//If countdwon reaches 0 
+      	if (number === 0) {
+    	//Call the stop function
+        stop();
+        //  Alert the user that time is up.
+        alert("Time Up!");
+        //Call score function
+            function stop() {
+
+      //  Clears our intervalId
+      //  We just pass the name of the interval
+      //  to the clearInterval function.
+      clearInterval(intervalId);
+    }
+
+
 //Function to indicate correct answer
  
 //Corresponding answers - use "input type="radio"
@@ -77,5 +117,9 @@ $("button").click(function(){
 //the function to update the question and possible answers
 //Function to show quiz is complete, show # of correct answers
 //# number of incorrect answers, # of unanswered questions
+
+
+      }
+    }
 
 });
